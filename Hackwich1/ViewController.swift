f17d78c2729875a72a6327581261d5627aa2a342
Ryan <ryan4652@hawaii.edu>
2019-01-10 10:12:31 -1000

    Initial Commit

---
+++ Hackwich1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackwich1
+//
+//  Created by Ryan B Domingo on 1/10/19.
+//  Copyright © 2019 Ryan B Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
